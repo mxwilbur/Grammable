c0f23b59a4a1661a12d3c1c8aa6201d03c1bfff8
mxwilbur <mxwilbur@outlook.com>
2019-08-05 23:17:16 +0000

    picture tdd

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,7 @@
 class Gram < ApplicationRecord
   validates :message, presence: true
+  validates :picture, presence: true
 
-  belongs_to :user
   mount_uploader :picture, PictureUploader
+  belongs_to :user
 end
