4839c973803ae1b8bfa1cadaa84a22397310b375
mxwilbur <mxwilbur@outlook.com>
2019-08-06 00:22:22 +0000

    fix 3 tdd errors

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe CommentsController, type: :controller do
       user = FactoryBot.create(:user)
       sign_in user
 
-      post :create,params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
+      post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
 
       expect(response).to redirect_to root_path
       expect(gram.comments.length).to eq 1
@@ -17,14 +17,14 @@ RSpec.describe CommentsController, type: :controller do
 
     it "should require a user to be logged in to comment on a gram" do
       gram = FactoryBot.create(:gram)
-      post :create, params: {gram_id, gram.id, comment: { messagge: 'awesome gram' } }
+      post :create, params: { gram_id: gram.id, comment: { messagge: 'awesome gram' } }
       expect(response).to redirect_to new_user_session_path
     end
 
     it "should return http status of not found if the gram isn't found" do
       user = FactoryBot.create(:user)
       sign_in user
-      post :create, params: {gram_id, 'YOLOSWAG', comment: { message: 'awesome gram' } }
+      post :create, params: {gram_id: 'YOLOSWAG', comment: { message: 'awesome gram' } }
       expect(response).to have_http_status :not_found
     end
   end
