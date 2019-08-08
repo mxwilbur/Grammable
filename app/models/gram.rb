adac867470eac95cefea161c2da8af898c195c39
mxwilbur <mxwilbur@outlook.com>
2019-08-08 15:37:32 +0000

    included dependent-destroy

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,7 +1,7 @@
 class Gram < ApplicationRecord
   validates :message, presence: true
   validates :picture, presence: true
-  has_many :comments
+  has_many :comments, dependent: :destroy
   
   mount_uploader :picture, PictureUploader
   belongs_to :user
